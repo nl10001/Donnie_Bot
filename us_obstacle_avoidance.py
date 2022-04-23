2420f3aee5fbaccc3cb4cb2fdc8a2e7d5ac58b51
apollock42 <99260918+apollock42@users.noreply.github.com>
2022-04-23 16:50:06 +0100

    Update us_obstacle_avoidance.py

--- us_obstacle_avoidance.py
+++ us_obstacle_avoidance.py
@@ -10,22 +10,24 @@ from geometry_msgs.msg import Twist
 
 class Donnie_Bot:
 	def __init__(self):
+		#initialising the nodes, publishers and subscribers for the motor values and the ultrasonic data
 		rospy.init_node('us_obstacle_avoidance_node')
-		rospy.on_shutdown(self.shutdownhook)
+		rospy.on_shutdown(self.shutdownhook)#runs shutdownhook code when the scripts is ended
 		self.ultrasonic_sub=rospy.Subscriber('/sonar', Float32, self.get_sonar)
 		self.pub=rospy.Publisher('/cmd_vel',Twist, queue_size=10)
 		self.sonar = Float32()
 		self.ctrl_c = False
 		print("obstacle avoidance using ultrasonic sensor is starting")
 		self.rate = rospy.Rate(10)
-
+	#subscribing to the /sonar topic to obtain the ultrasonic data
 	def get_sonar(self, msg):
 		self.sonar=msg
-
+	
 	def obstacleAvoidance(self):
 		self.var=Twist()
 		try:
 			while not self.ctrl_c:
+				#tells motors to turn depending if the ultrasonic senses an obstacle
 				if self.sonar.data <= 20:
 					self.var.angular.z=1
 				else:
@@ -37,7 +39,7 @@ class Donnie_Bot:
 			pass
 
 		rospy.spin()
-
+	#stops the motors when the script is ended
 	def shutdownhook(self):
 		print("ending...")
 		self.ctrl_c = True
